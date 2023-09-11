21af66fa497ede1e5dc2c13c2a5d8c67726857e7
ngranillo <n.granillo@alumno.um.edu.ar>
2023-09-11 00:10:47 -0300

    asar de turno

--- changelog.md
+++ changelog.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 
+## [0.0.9] - 2023-09-11
+
+### Added
+
+- giving turns function
+- tests for the function
+
 ## [0.0.8] - 2023-09-10
 
 ### Added
--- game/scrabble.py
+++ game/scrabble.py
@@ -12,3 +12,12 @@ class ScrabbleGame:
         self.dictionary = Dictionary('dictionaries/dictionary.txt')
         for _ in range(player_count):
             self.players.append(Player())
+            self.current_player = None
+            
+    def next_turn(self):
+        if self.current_player is None:
+            self.current_player = self.players[0]
+        elif id(self.current_player) == id(self.players[(len(self.players)) -1]):
+            self.current_player = self.players[0]
+        else:
+            self.current_player = self.players[self.players.index(self.current_player)+ 1]
